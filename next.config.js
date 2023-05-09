3daa90174bc56b5f12058907236ca1cd177265f8
Armand Morin <72650161+Armand-Morin@users.noreply.github.com>
2023-05-08 18:13:30 -0700

    u

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	output: 'export',
+	distDir: 'docs',
+};
+
+module.exports = {
+	env: {
+	},
+};
+
+module.exports = nextConfig;
